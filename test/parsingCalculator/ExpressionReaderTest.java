cef1e9014f5a7eee67d02cc563b6e2e6ee4f9dc4
sbohmann <mail@sebastian-bohmann.com>
2017-10-01 15:45:28 +0200

    Added a test case

--- test/parsingCalculator/ExpressionReaderTest.java
+++ test/parsingCalculator/ExpressionReaderTest.java
@@ -47,4 +47,18 @@ public class ExpressionReaderTest
             Assert.assertEquals(3.0, expression.initialValue, 0.0);
         }
     }
+    
+    @Test
+    public void simpleAddition()
+    {
+        for (String input : List.of("3+5", "3 + 5", " 3 + 5 ", "3 + (2 + 3)", " 3+( 2+3 ) "))
+        {
+            Expression expression = ExpressionReader.readExpression(input);
+            Assert.assertEquals(1, expression.operations.size());
+            Assert.assertEquals(3.0, expression.initialValue, 0.0);
+            Operation operation = expression.operations.get(0);
+            Assert.assertEquals(Operator.Addition, operation.operator);
+            Assert.assertEquals(5.0, operation.value, 0.0);
+        }
+    }
 }
